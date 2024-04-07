3f047bc9bf49208595337808edf527d935727c0e
PatelParth8 <parthpatel.imscit20@gmail.com>
2024-04-07 20:59:05 +0530

    first commit

---
+++ lib/database.dart
@@ -0,0 +1,102 @@
+import 'dart:convert';
+import 'package:untitled/view_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController numberController = TextEditingController();
+
+  Future<void> insertdata() async {
+    if (nameController.text != "" || numberController.text != "") {
+      try {
+        String uri = "http://10.0.2.2:8888/database/insert_record.php";
+        var res = await http.post(Uri.parse(uri), body: {
+          "name": nameController.text,
+          "number": numberController.text,
+        });
+        var response = await json.decode((res.body));
+
+        if (response["success"] == "true") {
+          print("Record Inserted");
+        } else {
+          print("Some Issues");
+        }
+      } catch (e) {
+        print(e);
+      }
+    } else {
+      print("Please Fill All Fields");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: const Center(child: Text("DataBase Connection")),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Enter Your Name",
+                prefixIcon: Icon(Icons.person),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              controller: numberController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Enter Your RollNumber",
+                prefixIcon: Icon(Icons.numbers),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: ElevatedButton.icon(
+              onPressed: () {
+                insertdata();
+                nameController.clear();
+                numberController.clear();
+              },
+              icon: const Icon(Icons.save_alt_sharp),
+              label: const Text("Save"),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: Builder(
+              builder: (context) {
+                return ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>const view_data()));
+                    }, child: const Text("View Data"));
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/view_data.dart
@@ -0,0 +1,90 @@
+import 'dart:convert';
+
+import 'package:untitled/update_record.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class view_data extends StatefulWidget {
+  const view_data({super.key});
+
+  @override
+  State<view_data> createState() => _view_dataState();
+}
+
+class _view_dataState extends State<view_data> {
+
+  List userdata = [];
+
+  Future<void> deleterecord(String name) async {
+    try
+    {
+      String uri = "http://10.0.2.2:8888/database/delete_record.php";
+
+      var res = await http.post(Uri.parse(uri), body: {"name":name});
+      var response = await json.decode((res.body));
+      if(response["success"]=="true")
+      {
+        print("Record Deleted");
+        getrecord();
+      }
+      else
+        {
+          print("Some Issues");
+        }
+    }
+        catch(e)
+        {
+          print(e);
+        }
+  }
+
+  Future<void> getrecord() async {
+    String uri = "http://10.0.2.2:8888/database/fetchapi.php";
+
+    try{
+      var response = await http.get(Uri.parse(uri));
+
+      setState(() {
+        userdata = json.decode((response.body));
+      });
+    }
+    catch(e){print(e);}
+  }
+
+  @override
+  void initState() {
+    getrecord();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("View Data"),
+      ),
+      body: ListView.builder(
+      itemCount: userdata.length,
+      itemBuilder: (context, index)
+        {
+          return Card(
+            margin: const EdgeInsets.all(10),
+            child: ListTile(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => update_record(userdata[index]["name"],userdata[index]["number"])));
+              },
+              title: Text(userdata[index]["name"]),
+              subtitle: Text(userdata[index]['number']),
+              trailing: IconButton(
+                onPressed: () {
+                  deleterecord(userdata[index]["name"]);
+                },
+                icon: const Icon(Icons.remove_circle),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
